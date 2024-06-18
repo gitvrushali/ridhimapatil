fcc6da7edfbfd87b920bbf72f87a8212b3fb266e
vrushali <vrushalipatil316@gmail.com>
2024-06-18 21:44:38 +0200

    testing new folder

---
+++ ridhimapatil/newjavaClass.cpp
@@ -0,0 +1,9 @@
+/*
+ * newjavaClass.cpp
+ *
+ *  Created on: 18-Jun-2024
+ *      Author: vrush
+ */
+
+#include "newjavaClass.h"
+
---
+++ ridhimapatil/newjavaClass.h
@@ -0,0 +1,18 @@
+/*
+ * newjavaClass.h
+ *
+ *  Created on: 18-Jun-2024
+ *      Author: vrush
+ *
+ *
+ *      kkkkkkkkkkkkkkkkkkkkkkkkk
+ *     ,,,,77777777777777777777777777777777777777777
+ */
+
+#ifndef NEWJAVACLASS_H_
+#define NEWJAVACLASS_H_
+
+class newjavaClass {
+};
+
+#endif /* NEWJAVACLASS_H_ */
